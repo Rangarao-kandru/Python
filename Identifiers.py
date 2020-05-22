04910a16b03923e5fbbe5ea5e23c44b5f3ab01db
learndevops <rangarao.info@gmail.com>
2020-05-23 02:35:56 +0530

    Identifiers

---
+++ Identifiers.py
@@ -0,0 +1,36 @@
+#Identifiers In Python
+#1. Identifier Name can contain Alphabets, Numbers(0-9) and Special character (_)
+
+a123=10                  #Valid
+print(a123)              #Valid
+
+#2. Identifier should not starts with Number
+
+a123=10                  #Valid
+print(a123)              
+
+
+#123a=20                 #Invalid
+#print(123a) 
+
+_a123=30                 # Valid
+print(_a123)             
+
+#3.Python Identifiers are case sensitive
+
+total=10
+TOTAL=20
+print(total)
+print(TOTAL)
+
+#4. Reserved words should not  use as Identifiers
+
+#if=10              #Invalid
+
+#print(if)      
+
+
+#5. there is no limit on length of Identifier
+
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=50
+print(XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
