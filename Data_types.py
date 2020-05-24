296cfeb80ee4472101d623c93ae7237455c61adc
learndevops <rangarao.info@gmail.com>
2020-05-25 00:00:55 +0530

    Data Types

---
+++ Data_types.py
@@ -0,0 +1,280 @@
+# Python data Types
+#int
+#float
+#complex
+#boolean
+#string
+#
+#
+#
+
+
+#int data type:
+print("*********************************************int Data Type*****************************************")
+print("x=12345")
+x=12345
+print("Data type of X:")
+print(type(x))
+print("Value of X:")
+print(x)
+print("********************02*************************")
+#Binary Value
+print("x=0b1111")
+x=0b1111
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************03*************************")
+#Binary Value
+print("x=0B1111")
+x=0B1111
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************04*************************")
+#Octal Value
+print("x=0o1234567")
+x=0o1234567
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************05***************************")
+#Octal Value
+print("x=0O1234567")
+x=0O1234567
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************06***************************")
+#Hexa Decimal Value
+print("x=0xface")
+x=0xface
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************07***************************")
+#Hexa Decimal Value
+print("x=0Xface")
+x=0Xface
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************08*************************")
+#Bin() function
+print("x=bin(1234)")
+x=bin(1234)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************09*************************")
+#Bin() function
+print("x=bin(0o2322)")
+x=bin(0o2322)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************10*************************")
+#Bin() function
+print("x=bin(0x2322)")
+x=bin(0x2322)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************11*************************")
+#oct() function
+print("x=oct(0b10011010010)")
+x=oct(0b10011010010)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************12*************************")
+#oct() function
+print("x=oct(12345)")
+x=oct(12345)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************13*************************")
+#oct() function
+print("x=oct(0xface)")
+x=oct(0xface)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************14*************************")
+#hex() function
+print("x=hex(0o1234567)")
+x=hex(0o1234567)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************15*************************")
+#hex() function
+print("x=hex(0b1111)")
+x=hex(0b1111)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************16*************************")
+#hex() function
+print("x=hex(12345)")
+x=hex(12345)
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("*********************************************float Data Type*****************************************")
+print("x=123.45")
+x=123.45
+print(type(x))
+print("value of X:")
+print(x)
+print("********************18*************************")
+print("x=1.2e3")
+x=1.2e3
+print("Data Type of x:")
+print(type(x))
+print("value of X:")
+print(x)
+
+print("*********************************************Complex Data Type*****************************************")
+print("x=10+20J")
+x=10+20J
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************20*************************")
+print("x=10.25+20.34J")
+x=10.25+20.34J
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************21*************************")
+print("x=0b1111+20.34J")
+x=0b1111+20.34J
+print("Data Type of x:")
+print(type(x))
+print("value of x:")
+print(x)
+print("********************22*************************")
+print("x=10+20J, y-20+30J")
+x=10+20J
+y=20+30J
+print("Data Type of x:")
+print(type(x))
+print("Data Type of y:")
+print(type(y))
+print("z=x+y")
+z=x+y
+print("value of z:")
+print(z)
+print("********************23*************************")
+print("x=10+20J, y-20+30J")
+x=10+20J
+y=20+30J
+print("Data Type of x:")
+print(type(x))
+print("Data Type of y:")
+print(type(y))
+print("z=x-y")
+z=x-y
+print("value of z:")
+print(z)
+print("********************24*************************")
+print("x=10+20J, y-20+30J")
+x=10+20J
+y=20+30J
+print("Data Type of x:")
+print(type(x))
+print("Data Type of y:")
+print(type(y))
+print("z=x*y")
+z=x*y
+print("value of z:")
+print(z)
+print("*********************************************Boolean Data Type*****************************************")
+print("x=True")
+x=True
+print("Type of X:")
+print(type(x))
+print("Value of X:")
+print(x)
+print("********************26*************************")
+print("y=False")
+y=False
+print("Type of y:")
+print(type(y))
+print("Value of y:")
+print(y)
+print("********************27*************************")
+print("y=True+True")
+y=True+True
+print("Type of y:")
+print(type(y))
+print("Value of y:")
+print(y)
+print("********************28*************************")
+print("y=True+False")
+y=True+False
+print("Type of y:")
+print(type(y))
+print("Value of y:")
+print(y)
+print("*********************************************String Data Type*****************************************")
+print("x=This is Python Program")
+x='This is Python Program'
+print("Type of x:")
+print(type(x))
+print("Value of X:")
+print(x)
+print("********************30*************************")
+print("x=This is Python Program")
+x="This is Python Program"
+print("Type of x:")
+print(type(x))
+print("Value of X:")
+print(x)
+print("********************31*************************")
+print("x='''This is Python Program'''")
+x='''This is "Python" Program'''
+print("Type of x:")
+print(type(x))
+print("Value of X:")
+print(x)
+print("********************32*************************")
+print("x='''This is Python Program'''")
+x="""This is "Python" Program"""
+print("Type of x:")
+print(type(x))
+print("Value of X:")
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
