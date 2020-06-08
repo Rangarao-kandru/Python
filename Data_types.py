4beb464a93421545d6c33b409d1ad8d83965457f
learndevops <rangarao.info@gmail.com>
2020-06-08 14:02:52 +0530

    Practice 01

--- Data_types.py
+++ Data_types.py
@@ -535,31 +535,471 @@ print("x=10+20J")
 x=10+20J
 print("str(x)")
 print(str(x))
+print("********************Immutability*************************")
+print("Immutable - Object Value Can't be changed")
+print("x=10, y=10")
+x=10
+y=10
+print("id(x), id(y)")
+print(id(x))
+print(id(y))
+print("Both the Variables are referencing the same object")
+print("********************76*************************")
+print("Immutable - Object Value Can't be changed")
+print("x=10, y=10, z=20")
+x=10
+y=10
+z=20
+print("id(x), id(y), id(z)")
+print(id(x))
+print(id(y))
+print(id(z))
+print("y=20")
+y=20
+print("id(y)")
+print(id(y))
+print("here two objects were created. when y value is changed from 10 to 20, Python Interpreter search the new value(20) and point the Y to Existing Object ")
+print("********************77*************************")
+print("Immutable - Object Value Can't be changed")
+print("x=10, y=10")
+x=10
+y=10
+print("id(x), id(y)")
+print(id(x))
+print(id(y))
+print("Other way to check the x and Y is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+print("********************78*************************")
+print("Immutable - Int - Limitations")
+print("x=258, y=258")
+x=258
+y=258
+print("id(x), id(y)")
+print(id(x))
+print(id(y))
+print("Other way to check the x and Y is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+print("********************79*************************")
+print("Immutable - float - Limitations")
+print("x=10.5, y=10.5")
+x=10.5
+y=10.5
+print("id(x), id(y)")
+print(id(x))
+print(id(y))
+print("Other way to check the x and Y is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+print("********************80*************************")
+print("Immutable - complex - Limitations")
+print("p=10+20J, q=10+20J")
+p=1+2J
+q=1+2J
+print(type(p))
+print(type(q))
+print("id(p), id(q)")
+print(id(p))
+print(id(q))
+print("Other way to check the p and q is:")
+print("p is q")
+print(p is q)
+print("q is p")
+print(q is p)
+print("********************81*************************")
+print("Immutable - Boolean - No limitations")
+print("x=True, y=True")
+x=True
+y=True
+print(type(x))
+print(type(y))
+print("id(x), id(y)")
+print(id(x))
+print(id(y))
+print("Other way to check the p and q is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+print("********************82*************************")
+print("Immutable - String - No limitations")
+print("x='ranga', y='ranga',z='rangarao'")
+x='ranga'
+y='ranga'
+z='rangarao'
+print(type(x))
+print(type(y))
+print(type(z))
+print("id(x), id(y) , id(z)")
+print(id(x))
+print(id(y))
+print(id(z))
+print("Other way to check the p and q is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+
+print("Immutable - String - No limitations")
+print("x='ranga', y='ranga',z='rangarao'")
+x='ranga'
+y='ranga'
+z='rangarao'
+print(type(x))
+print(type(y))
+print(type(z))
+print("id(x), id(y) , id(z)")
+print(id(x))
+print(id(y))
+print(id(z))
+print("Other way to check the p and q is:")
+print("x is y")
+print(x is y)
+print("y is x")
+print(y is x)
+print("y='ranga'")
+y='rangarao'
+print("id(y)")
+print(id(y))
+print("z is y")
+print(z is y)
+print("********************83*******Bytes Data Type******************")
+print("Bytes Data Type")
+print("      Bytes data type is immutable")
+print("     Bytes data type is supports values between 0 to 256")
+print("x=[10, 20, 30, 10]")
+print("p=bytes(x)")
+x=[10, 20, 30, 10]
+p=bytes(x)
+print("type(x), type(p)")
+print(type(x))
+print(type(p))
+print("p")
+print(p)
+print("for in p: print(i)")
+for i in p: print(i)
+print("p[1]")
+print(p[1])
+
+print("p[0]")
+print(p[0])
+
+print("p[-1]")
+print(p[-1])
+
+print("p[0:3]")
+for i in p[0:3]: print(i)
+
+print("x=[10, 20, 30, 257]")
+print("p=bytes(x)")
+x=[10, 20, 30, 257]
+#p=bytes(x)
+#print("for in p: print(i)")
+#for i in p: print(i)
+print("ValueError: bytes must be in range(0, 256)")
 
 
+print("x=[10, 20, 30, 256]")
+print("p=bytes(x)")
+x=[10, 20, 30, 255]
+p=bytes(x)
+print("p[0]")
+print(p[0])
+print("p[0]=100")
+#p[0]=100
+print("TypeError: 'bytes' object does not support item assignment")
 
 
+print("********************84*******Bytearray Data Type******************")
+print("Bytearray Data Type:")
+print("     Bytearray data type is mutable")
+print("     Bytearray data type is supports values between 0 to 256")
+print("x=[10, 20, 30, 255]")
+print("p=bytearray(x)")
+x=[10, 20, 30, 255]
+p=bytearray(x)
+print("type(x), type(p)")
+print(type(x))
+print(type(p))
+print("p")
+print(p)
+print("for in p: print(i)")
+for i in p: print(i)
+print("p[1]")
+print(p[1])
+
+print("p[0]")
+print(p[0])
+
+print("p[-1]")
+print(p[-1])
+
+print("for i in p[0:3]: print(i)")
+for i in p[0:3]: print(i)
 
 
+print("p[0]=100")
+p[0]=100
+print("for i in p: print(i)")
+for i in p: print(i)
+      
+print("x=[10, 20, 30, 256]")
+print("p=bytearray(x)")
+x=[10, 20, 30, 256]
+#p=bytearray(x)
+print("ValueError: byte must be in range(0, 256)")
 
 
+print("********************85*************************")
+print("Bytearray Data Type:")
+print("     Bytearray data type is mutable")
+print("     Bytearray data type is supports values between 0 to 256")
+print("x=[10, 20, 30, 255]")
+print("p=bytearray(x)")
+x=[10, 20, 30, 255]
+p=bytearray(x)
+print("type(p)")
+print(type(p))
+
+print("for i in p: print(i)")
+for i in p: print(i)
+
+print("p[0]")
+print(p[0])
+
+print("p[-1]")
+print(p[-1])
+
+print("for i in p[0:3]: print(i)")
+for i in p[0:3]: print(i)
+
+print("p[0]=100")
+print("for i in p: print(i)")
+p[0]=100
+for i in p: print(i)
+
+print("p.append(40)")
+print("for i in p: print(i)")
+p.append(40)
+for i in p: print(i)
+
+print("p.remove(40)")
+print("for i in p: print(i)")
+p.remove(40)
+for i in p: print(i)
+
+print("********************86**********List Data Type***************")
+print("List Data Type:")
+print("     Insertion ordered is preserved, duplicates are allowed")
+print("     Bytearray data type is mutable")
+print("     list values are represented in []")
+print("     Heterogenous Values are allowed")
+print("x=[10, 20, 30, 10]")
+print("type(x)")
+x=[10, 20, 30, 10]
+print(type(x))
+
+print("x")
+print(x)
+
+print("for i in x; print(i)")
+for i in x: print(i)
+
+print("x.append(40)")
+x.append(40)
+print("x")
+print(x)
+
+print("x.append('ranga')")
+x.append('ranga')
+print("x.append(None)")
+x.append(None)
+print("x")
+print(x)
+
+print("x.remove(10)")
+x.remove(10)
+print("x")
+print(x)
+
+print("x1=x*2")
+x1=x*2
+print("x1")
+print(x1)
+
+print("x[0]=60")
+x[0]=60
+print("x")
+print(x)
+
+print("********************87******Tuple Data Type*******************")
+print("Tuple Data Type:")
+print("     Insertion ordered is preserved, duplicates are allowed")
+print("     Bytearray data type is Immutable")
+print("     list values are represented in ()")
+print("     Heterogenous Values are allowed")
+print("x=(10, 20, 30, 10)")
+print("type(x)")
+x=(10, 20, 30, 10)
+print(type(x))
+
+print("x")
+print(x)
+
+print("for i in x; print(i)")
+for i in x: print(i)
+
+print("x[1]")
+print(x[1])
+
+print("x[-1]")
+print(x[-1])
+
+print("x[0:3]")
+print(x[0:3])
+
+print("x1=x*2")
+print(x1)
 
 
+print("x[0]=40")
+#x[0]=40
+print("TypeError: 'tuple' object does not support item assignment")
 
 
+print("x.append(40)")
+print("AttributeError: 'tuple' object has no attribute 'append'")
+#x.append(40)
+#print("x")
+#print(x)
 
 
+print("********************88*******Range Data Type******************")
+print("Range Data Type:")
+print("     it is used to represent range of values like Form 1: range(x) or form 2: range(x,y) or Form 3: rangae(x,y,z)")
+print("     Bytearray data type is Immutable")
+print("     Only Numerical Values are allowed")
+
+print("Form 1:")
+print("range(10) - It represent the values from 0 to 9")
+print(range(10))
+print('for i in range(10): print(i)')
+for i in range(10): print(i)
+
+print("Form 2:")
+print("range(10,20) - It represent the values from 10 to 19")
+print(range(10,20))
+print('for i in range(10,20): print(i)')
+for i in range(10,20): print(i)
 
 
+print("Form 3:")
+print("range(10,40,5) - It represent the values from 10 to 39, with a step value of 5")
+print(range(10,40,5))
+print('for i in range(10,40,5): print(i)')
+for i in range(10,40,5): print(i)
 
 
+print("range(0)=777")
+#range[0]=777
+print("TypeError: 'type' object does not support item assignment")
 
 
+print("********************89*******set Data Type******************")
+print("set Data Type:")
+print("     it is used to store list of values, it doesn't preserve the Insertion order and duplicates are not allowed")
+print("     set data type is mutable")
+print("     it presents the data using {}")
+print("     it doesn't support Indexing and slicing")
+print("     it supports heterogenous values")
+
+print("x={10,20,30,10,20,30}")
+x={10,20,30,10,20,30}
+print("Type(x)")
+print(type(x))
+print("x:")
+print(x)
 
 
+#print(x[0])
+print("TypeError: 'set' object is not subscriptable")
 
 
+print("x.add(40)")
+x.add(40)
+print('x')
+print(x)
 
+print("x.remove(40)")
+x.remove(40)
+print('x')
+print(x)
+
+print("x.add('ranga')")
+x.add('ranga')
+print('x')
+print(x)
+
+print("********************90*******frozenset Data Type******************")
+print("frozenset Data Type:")
+print("     it is used to store list of values, it doesn't preserve the Insertion order and duplicates are not allowed")
+print("     set data type is Immutable")
+print("     it presents the data using {}")
+print("     it doesn't support Indexing and slicing")
+print("     it supports heterogenous values")
+
+print("p={10,20,30,40}")
+print("m=frozenset(p)")
+p={10,20,30,40}
+m=frozenset(p)
+print(type(m))
+print(m)
+
+
+print("m.add('ranga')")
+#m.add('ranga')
+print("AttributeError: 'frozenset' object has no attribute 'add'")
+
+print("m.remove(10)")
+#m.remove(10)
+print("AttributeError: 'frozenset' object has no attribute 'remove'")
+
+
+print("********************91*******Dictionary Data Type******************")
+print("Dict Data Type:")
+print("     it is used to store key-pair values; duplicates are not allowed in Key values, duplicates are allowed in Pair values")
+print("     set data type is mutable")
+print("     it presents the data using {}")
+
+
+print("d={}")
+d={} # empty dict. by defauly python make the empty set as dictionary data type
+print(type(d))
+
+#To create a empty set data type
+print("p=set()")
+p=set()
+print("type(p)")
+print(type(p))
+
+
+print("d={100:'ranga', 200:'vijaya', 300:'corona'}")
+d={100:'ranga', 200:'vijaya', 300:'corona'}
+print("d")
+print(d)
+
+
+print("d[100]='rangarao'")
+d[100]='rangarao'
+print("d")
+print(d)
 
 
 
